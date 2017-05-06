c2d4c0cee2c8b64bd213e3d2359cd83f3c77c604
Yassine Khachlek <yassine.khachlek@gmail.com>
2017-05-07 00:51:44 +0100

    Code cleaning.

--- src/app/Http/Controllers/InstallController.php
+++ src/app/Http/Controllers/InstallController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class InstallController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the install form.
      *
      * @return \Illuminate\Http\Response
      */
@@ -44,17 +44,7 @@ class InstallController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Save the .env file and process migration and seeding.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -135,50 +125,5 @@ class InstallController extends Controller
                 'output' => $output
             )
         );
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Request $Request, $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Request $Request, $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Request $Request, $id)
-    {
-        //
     }
 }
